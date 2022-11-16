aa51e77ced16b6e53581594046468fdfe4a7df0c
@hexlet/cli <support@hexlet.io>
2022-11-16 22:57:33 +0300

    submit java-collections-ru/generics

---
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -0,0 +1,36 @@
+package exercise;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+// BEGIN
+class App {
+
+	    public static List<Map> findWhere(List<Map> books, Map<String, String> where) {
+
+		            List<Map> result = new ArrayList<>();
+
+			            Integer sizeWhere = where.size();
+
+				            for (Map book: books) {
+						                Integer count = 0;
+								            for (Map.Entry<String, String> w: where.entrySet()) {
+										                    Object valueW = w.getValue();
+												                    Object keyW = w.getKey();
+														                    Object valueB = book.get(keyW);
+
+																                    if (valueW.equals(valueB)) {
+																			                        count++;
+																						                }
+																		                }
+									                if (count.equals(sizeWhere)) {
+												                result.add(book);
+														            }
+											        }
+					            return result;
+						        }
+
+}
+//END
